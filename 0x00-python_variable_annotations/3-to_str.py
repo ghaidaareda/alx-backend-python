02d2033d0d82fc5798abd528d43a284c016199c4
ghaidaareda <dodo.sa7afy@gmail.com>
2024-01-11 16:17:02 +0200

    3-to_str.py

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-type-annotated function to_str 
-that takes a float n as argument 
+type-annotated function to_str
+that takes a float n as argument
 and returns the string representation of the float
 """
 import math
@@ -9,5 +9,4 @@ import math
 
 def to_str(n: float) -> str:
     """returns the string representation of the float """
-    floor_result = math.floor(n)
-    return str(floor_result)
+    return str(n)
