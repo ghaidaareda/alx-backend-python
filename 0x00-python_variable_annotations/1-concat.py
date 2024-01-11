0e91b2c87614b96f4c8bc3941dec31466dd3c910
ghaidaareda <dodo.sa7afy@gmail.com>
2024-01-11 14:44:47 +0200

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type-annotated function concat
+str1 and a string str2 as arguments
+and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concat of str1 &str2 """
+    return str1 + str2
