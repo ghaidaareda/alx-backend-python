cf85ded456d926db4e1625d06b34ea219e14883c
ghaidaareda <dodo.sa7afy@gmail.com>
2024-01-12 02:23:21 +0200

    7-to_kv.py

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+ type-annotated function to_kv
+ that takes a string k and an int OR float v as arguments
+ and returns a tuple
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple containing the key-value pair with the value squared."""
+    return (k, v * v)
