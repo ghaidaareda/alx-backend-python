976ffb443ad5c38043cf09b7554e108d66d71db8
ghaidaareda <dodo.sa7afy@gmail.com>
2024-01-12 03:57:37 +0200

    102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """
-Use mypy to validate the following piece of code 
+Use mypy to validate the following piece of code
 and apply any necessary changes.
 """
-from typing import Any, List, Union, SupportsIndex, Sequence
-def zoom_array(lst: List[int], factor: Any= 2) -> List[Any]:
+from typing import Any, List, Union
+
+
+def zoom_array(lst: List[int], factor: Any = 2) -> List[Any]:
+    """ zoom array"""
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
