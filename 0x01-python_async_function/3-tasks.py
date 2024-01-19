eab9dad5e282ca00cf85bc7c631b57b2175f6cb3
ghaidaareda <dodo.sa7afy@gmail.com>
2024-01-19 22:45:05 +0200

    3-tasks.py

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+The purpose of the task_wait_random function is to
+create and return an asyncio.Task object
+that represents the asynchronous execution
+of the wait_random coroutine with a specified max_delay.
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """function returns a asyncio.Task"""
+    async def create_async():
+        return await wait_random(max_delay)
+    return asyncio.create_task(create_async())
