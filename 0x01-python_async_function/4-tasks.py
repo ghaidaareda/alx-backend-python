35f67b772e4022c8d18c79673ce4c7aa4d72fcb0
ghaidaareda <dodo.sa7afy@gmail.com>
2024-01-19 23:01:51 +0200

    4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -11,5 +11,6 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """run multiple asynchronous tasks concurrently, in parallel."""
-    delays = await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
+    delays = await asyncio.gather(*(task_wait_random(max_delay)
+                                    for _ in range(n)))
     return sorted(delays)
