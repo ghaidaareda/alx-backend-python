a29f0e6f63b569d81011dcb2ac327eee1d6c912a
ghaidaareda <dodo.sa7afy@gmail.com>
2024-01-20 21:46:49 +0200

    1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
-'''Asynchronous Python
-'''
+"""
+Async Comprehensions:
+used to iterate over an asynchronous iterable
+(such as an asynchronous generator)
+and create a new asynchronous iterable
+based on some transformation.
+"""
+
+
 from typing import List
-
-
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    '''make a list of 10 num from imported generator
-    '''
-    return [num async for num in async_generator()]
+    """
+    The syntax for async comprehensions is similar to
+    regular comprehensions but includes the async keyword.
+    """
+    return [value async for value in async_generator()]
