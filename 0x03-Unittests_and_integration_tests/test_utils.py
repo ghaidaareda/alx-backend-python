906b077341dea442ed9881246dc2d441c0b120c5
ghaidaareda <dodo.sa7afy@gmail.com>
2024-05-11 22:31:37 +0300

    4

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -38,20 +38,21 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
             
     class TestGetJson(unittest.TestCase):
-    @parameterized.expand([
+        """ test get-json"""
+        @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-    def test_get_json(
-            self,
-            test_url: str,
-            test_payload: Dict,
-    ) -> None:
-        """Tests `get_json`'s output."""
-        attrs = {'json.return_value': test_payload}
-        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
-            self.assertEqual(get_json(test_url), test_payload)
-            req_get.assert_called_once_with(test_url)
+        def test_get_json(
+                self,
+                test_url,
+                test_payload,
+        ) -> None:
+            """Tests `get_json`'s output."""
+            attrs = {'json.return_value': test_payload}
+            with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+                self.assertEqual(get_json(test_url), test_payload)
+                req_get.assert_called_once_with(test_url)
 
 if __name__ == "__main__":
     unittest.main()
