31de4a33ce33c1bc1903efb754d566ee601e8afc
ghaidaareda <dodo.sa7afy@gmail.com>
2024-05-11 22:14:52 +0300

    3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,8 +5,9 @@ first unit test for utils.access_nested_map.
 from multiprocessing import context
 from typing import Dict, Tuple
 import unittest
+from unittest.mock import Mock, patch
 from parameterized import parameterized 
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 class TestAccessNestedMap(unittest.TestCase):
     """
@@ -35,6 +36,21 @@ class TestAccessNestedMap(unittest.TestCase):
         """Tests exception raising."""
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+            
+    class TestGetJson(unittest.TestCase):
+    @patch('utils.requests.get')
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_get.return_value = Mock()
+        mock_get.return_value.json.return_value = test_payload
+
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 if __name__ == "__main__":
     unittest.main()
