d0abc5fbf36575d35c7610339c7d07a559ea572f
ghaidaareda <dodo.sa7afy@gmail.com>
2024-05-13 21:41:40 +0300

    test-utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,7 +12,7 @@ from parameterized import parameterized
 
 class TestAccessNestedMap(unittest.TestCase):
     """
-    Tests the access_nested_map function
+    Tests access_nested_map function
     """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -22,11 +22,7 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map: Mapping,
                                path: Sequence, expected: int) -> None:
         """
-        Test the access_nested_map method.
-        Args:
-            nested_map (Dict): A dictionary that may have nested dictionaries
-            path (List, tuple, set): Keys to get to the required value in the
-                                     nested dictionary
+        Test access_nested_map method.
         """
         response = access_nested_map(nested_map, path)
         self.assertEqual(response, expected)
@@ -38,11 +34,7 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map: Mapping,
                                          path: Sequence) -> None:
         """
-        Test the access_nested_map method raises an error when expected to
-        Args:
-            nested_map (Dict): A dictionary that may have nested dictionaries
-            path (List, tuple, set): Keys to get to the required value in the
-                                     nested dictionary
+        Test the access_nested_map method error exception
         """
         with self.assertRaises(Exception):
             access_nested_map(nested_map, path)
@@ -50,7 +42,7 @@ class TestAccessNestedMap(unittest.TestCase):
 
 class TestGetJson(unittest.TestCase):
     """
-    Test the get_json function
+    Test get_json function
     """
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
@@ -59,10 +51,7 @@ class TestGetJson(unittest.TestCase):
     @patch("requests.get")
     def test_get_json(self, test_url, test_payload, mock_requests_get):
         """
-        Test the get_json method to ensure it returns the expected output.
-        Args:
-            url: url to send http request to
-            payload: expected json response
+        Test the get_json method
         """
         mock_requests_get.return_value.json.return_value = test_payload
         result = get_json(test_url)
@@ -72,8 +61,9 @@ class TestGetJson(unittest.TestCase):
 
 class TestMemoize(unittest.TestCase):
     """
-    Test the memoization decorator, memoize
+    Test memoization decorator & memoize
     """
+
     def test_memoize(self):
         """
         Test that utils.memoize decorator works as intended
