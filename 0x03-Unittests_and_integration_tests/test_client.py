eea5f8d387fda0cb80f3fed933c169ea45458a34
ghaidaareda <dodo.sa7afy@gmail.com>
2024-05-13 21:37:35 +0300

    client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -7,7 +7,7 @@ from client import GithubOrgClient
 
 class TestGithubOrgClient(unittest.TestCase):
     """
-    Test the GithubOrgClient class methods
+    Test GithubOrgClient class
     """
     @parameterized.expand([
         ("google"),
@@ -16,7 +16,7 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json', return_value={"payload": True})
     def test_org(self, org, mock_org):
         """
-        Test TestGithubOrgClient's org method
+        Test TestGithubOrgClient's org
         """
         org_test = GithubOrgClient(org)
         test_response = org_test.org
@@ -33,21 +33,22 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_pro.return_value = {"repos_url": "89"}
             test_org = GithubOrgClient('holberton')
             test_repo_url = test_org._public_repos_url
-            self.assertEqual(test_repo_url, mock_pro.return_value.get('repos_url'))
+            self.assertEqual(test_repo_url,
+                             mock_pro.return_value.get('repos_url'))
             mock_pro.assert_called_once()
 
-    @patch('client.get_json', return_value=[{'name': 'jil'},
+    @patch('client.get_json', return_value=[{'name': 'dodo'},
                                             {'name': 'fw'},
                                             {'name': 'sher'}])
     def test_public_repos(self, mock_repo):
         """
-        Test GithubOrgClient's public_repos method
+        Test GithubOrgClient's public_repos
         """
         with patch.object(GithubOrgClient,
                           '_public_repos_url',
                           new_callable=PropertyMock,
                           return_value="https://api.github.com/") as mock_pro:
-            test_client = GithubOrgClient('jil')
+            test_client = GithubOrgClient('dodo')
             test_repo = test_client.public_repos()
             for idx in range(3):
                 self.assertIn(mock_repo.return_value[idx]['name'], test_repo)
@@ -62,6 +63,6 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         GithubOrgClient's has_license method
         """
-        test_instance = GithubOrgClient('jil')
+        test_instance = GithubOrgClient('dodo')
         license_available = test_instance.has_license(repo, license_key)
         self.assertEqual(license_available, expected)
